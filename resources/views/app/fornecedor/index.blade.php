ac072067f408998733ea932a1b2ae231031fc827
jhoni-costa <jhoni_costa@outlook.com>
2022-04-18 22:00:52 -0300

    Blade @fi@else

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,3 +1,18 @@
 <h3>Fornecedor</h3>
 
-{{-- Fica o comentário que será descartado pelo blade --}}
+{{-- Fica o comentário que será descartado pelo interpretador do blade --}}
+
+@php
+
+@endphp
+
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem varios fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
+
+@dd($fornecedores)
