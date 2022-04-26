dbb7cae459046f87c32d2fd0bd4acb4532f19b08
Jhoni Regis Souza da Costa <jhoni_costa@outlook.com>
2022-04-26 20:03:50 -0300

    Blade - @isset

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -7,12 +7,14 @@
 @endphp
 
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem varios fornecedores cadastrados</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados</h3>
-@endif
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome']}}
+    <br>
+    Status: {{ $fornecedores[0]['status']}}
+    <br>
+    @isset($fornecedores[1]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj']}}
+    @endisset
+@endisset;
 
 @dd($fornecedores)
