df4e5760887bb6e10b21994e5bd69cc966bf0c5e
jhoni-costa <jhoni_costa@outlook.com>
2022-05-31 20:54:49 -0300

    Enviando parâmetros para componentes

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,19 +1,21 @@
+{{ $slot }}
+
 <form action='{{ route('site.contato') }}' method="POST">
     @csrf
-    <input type="text" placeholder="Nome" class="borda-preta" name='nome'>
+    <input type="text" placeholder="Nome" class="{{ $classe }}" name='nome'>
     <br>
-    <input type="text" placeholder="Telefone" class="borda-preta" name='telefone'>
+    <input type="text" placeholder="Telefone" class="{{ $classe }}" name='telefone'>
     <br>
-    <input type="text" placeholder="E-mail" class="borda-preta" nome='email'>
+    <input type="text" placeholder="E-mail" class="{{ $classe }}" nome='email'>
     <br>
-    <select class="borda-preta" name='motivo_contato'>
+    <select class="{{ $classe }}" name='motivo_contato'>
         <option value="">Qual o motivo do contato?</option>
         <option value="1">Dúvida</option>
         <option value="2">Elogio</option>
         <option value="3">Reclamação</option>
     </select>
     <br>
-    <textarea class="borda-preta" name='mensagem' placeholder='Preencha aqui a sua mensagem'></textarea>
+    <textarea class="{{ $classe }}" name='mensagem' placeholder='Preencha aqui a sua mensagem'></textarea>
     <br>
-    <button type="submit" class="borda-preta">ENVIAR</button>
+    <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
