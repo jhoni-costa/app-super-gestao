50f18ca1bc866e73b10ea39b5d78c9951eda3daf
jhoni-costa <jhoni_costa@outlook.com>
2022-05-31 20:45:14 -0300

    Usando @componets

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -24,25 +24,8 @@
 
     <div class="informacao-pagina">
         <div class="contato-principal">
-            <form action='{{ route('site.contato') }}' method="POST">
-                @csrf
-                <input type="text" placeholder="Nome" class="borda-preta" name='nome'>
-                <br>
-                <input type="text" placeholder="Telefone" class="borda-preta" name='telefone'>
-                <br>
-                <input type="text" placeholder="E-mail" class="borda-preta" nome='email'>
-                <br>
-                <select class="borda-preta" name='motivo_contato'>
-                    <option value="">Qual o motivo do contato?</option>
-                    <option value="1">Dúvida</option>
-                    <option value="2">Elogio</option>
-                    <option value="3">Reclamação</option>
-                </select>
-                <br>
-                <textarea class="borda-preta" name='mensagem' placeholder='Preencha aqui a sua mensagem'></textarea>
-                <br>
-                <button type="submit" class="borda-preta">ENVIAR</button>
-            </form>
+            @component('site.layouts._components.form_contato')
+            @endcomponent
         </div>
     </div>  
 </div>
