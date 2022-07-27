e3671f015f1d3ce2c22776cb7a82ef73c004f1ac
jhoni-costa <jhoni_costa@outlook.com>
2022-07-26 22:17:23 -0300

    Factorys

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -4,5 +4,5 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model {
-
+    protected $fillable = ['nome', 'telefone', 'email','motivo_contato','mensagem'];
 }
