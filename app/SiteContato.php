ea380e6a45cf07d86ac2d5ad66bb1c91359e3be1
jhoni-costa <jhoni_costa@outlook.com>
2022-08-10 20:30:17 -0300

    Ajustes site_contatos

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -4,5 +4,5 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model {
-    protected $fillable = ['nome', 'telefone', 'email','motivo_contato','mensagem'];
+    protected $fillable = ['nome', 'telefone', 'email','motivo_contatos_id','mensagem'];
 }
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -8,10 +8,10 @@
     <br>
     <input type="text" placeholder="E-mail" class="{{ $classe }}" value='{{ old('email') }}' name='email'>
     <br>
-    <select class="{{ $classe }}" name='motivo_contato'>
+    <select class="{{ $classe }}" name='motivo_contatos_id'>
         <option value="">Qual o motivo do contato?</option>
         @foreach ( $motivo_contatos as $key => $value )
-            <option value="{{ $value->id }}" {{ $value->id == old('motivo_contato') ? 'selected' : ""}}>{{ $value->motivo_contato }}</option>
+            <option value="{{ $value->id }}" {{ $value->id == old('motivo_contatos_id') ? 'selected' : ""}}>{{ $value->motivo_contato }}</option>
         @endforeach
     </select>
     <br>
@@ -19,8 +19,8 @@
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
-{{-- <div style='position:absolute; top:0px; left:0px; width:100%; background:red'> --}}
-    {{-- <pre> --}}
-    {{-- {{ print_r($errors); }} --}}
-    {{-- </pre> --}}
-{{-- </div> --}}
+<div style='position:absolute; top:0px; left:0px; width:100%; background:red'>
+    <pre>
+    {{ print_r($errors); }}
+    </pre>
+</div>
