8c4887651127f5c878852786a12487b255de1f18
jhoni-costa <jhoni_costa@outlook.com>
2022-08-16 21:59:49 -0300

    Implementando Middleware no Controller

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,12 @@ Route::get('/', function () {
 Route::get('/',[\App\HTTP\Controllers\PrincipalController::class,'principal'])
     ->name('site.index')
     ->middleware(LogAcessoMiddleware::class);
-Route::get('/sobre-nos',[\App\HTTP\Controllers\SobreNosController::class,'sobreNos'])
-    ->name('site.sobrenos')
+
+Route::get('/sobre-nos',[\App\HTTP\Controllers\SobreNosController::class,'sobreNos'])->name('site.sobrenos');
+
+    Route::get('/contato',[\App\HTTP\Controllers\ContatoController::class,'contato'])
+    ->name('site.contato')
     ->middleware(LogAcessoMiddleware::class);
-Route::get('/contato',[\App\HTTP\Controllers\ContatoController::class,'contato'])->name('site.contato');
 // Route::post('/contato',[\App\HTTP\Controllers\ContatoController::class,'contato'])->name('site.contato');
 Route::post('/contato',[\App\HTTP\Controllers\ContatoController::class,'salvar'])->name('site.contato');
 Route::get('/login',function(){return 'Login';})->name('site.login');
