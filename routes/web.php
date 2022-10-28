9ee2d2a4c40a2abec840dd9164a3faf4049fe2d2
Jhoni Costa <jhoni_costa@outlook.com>
2022-10-27 22:32:39 -0300

    Iniciando CRUD de Fornecedores

--- routes/web.php
+++ routes/web.php
@@ -16,29 +16,33 @@ Route::get('/', function () {
     return "<h1>Olá mundo!</h1>";
 });
 */
-Route::get('/',[\App\HTTP\Controllers\PrincipalController::class,'principal'])->name('site.index');
-Route::get('/sobre-nos',[\App\HTTP\Controllers\SobreNosController::class,'sobreNos'])->name('site.sobrenos');
-Route::get('/contato',[\App\HTTP\Controllers\ContatoController::class,'contato'])->name('site.contato');
+
+Route::get('/', [\App\HTTP\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [\App\HTTP\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [\App\HTTP\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 // Route::post('/contato',[\App\HTTP\Controllers\ContatoController::class,'contato'])->name('site.contato');
-Route::post('/contato',[\App\HTTP\Controllers\ContatoController::class,'salvar'])->name('site.contato');
+Route::post('/contato', [\App\HTTP\Controllers\ContatoController::class, 'salvar'])->name('site.contato');
 
-Route::get('/login/{erro?}',[\App\HTTP\Controllers\LoginController::class, 'index'])->name('site.login');
-Route::post('/login',[\App\HTTP\Controllers\LoginController::class, 'autenticar'])->name('site.login');
+Route::get('/login/{erro?}', [\App\HTTP\Controllers\LoginController::class, 'index'])->name('site.login');
+Route::post('/login', [\App\HTTP\Controllers\LoginController::class, 'autenticar'])->name('site.login');
 
-Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(function(){
-    // middleware('log.acesso','autenticacao')
-    Route::get('/home',[\App\HTTP\Controllers\HomeController::class,'index'])->name('app.home');
-    Route::get('/sair',[\App\HTTP\Controllers\LoginController::class,'sair'])->name('app.sair');
-    Route::get('/cliente',[\App\HTTP\Controllers\ClienteController::class,'index'])->name('app.cliente');
-    Route::get('/fornecedore',[\App\HTTP\Controllers\FornecedorController::class,'index'])->name('app.fornecedor');
-    Route::get('/produto',[\App\HTTP\Controllers\ProdutoController::class,'index'])->name('app.produto');
+Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(function () {
+    Route::get('/home', [\App\HTTP\Controllers\HomeController::class, 'index'])->name('app.home');
+    Route::get('/sair', [\App\HTTP\Controllers\LoginController::class, 'sair'])->name('app.sair');
+    Route::get('/cliente', [\App\HTTP\Controllers\ClienteController::class, 'index'])->name('app.cliente');
+
+    Route::get('/fornecedor', [\App\HTTP\Controllers\FornecedorController::class, 'index'])->name('app.fornecedor');
+    Route::post('/fornecedor/listar', [\App\HTTP\Controllers\FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
+    Route::get('/fornecedor/adicionar', [\App\HTTP\Controllers\FornecedorController::class, 'adicionar'])->name('app.fornecedor.adicionar');
+
+    Route::get('/produto', [\App\HTTP\Controllers\ProdutoController::class, 'index'])->name('app.produto');
 });
 
-Route::get('/teste/{p1}/{p2}',[\App\HTTP\Controllers\TesteController::class,'teste'])->name('teste');
+Route::get('/teste/{p1}/{p2}', [\App\HTTP\Controllers\TesteController::class, 'teste'])->name('teste');
 
 
-Route::fallback(function(){
-    echo "A rota acessada não existe: <a href=".route('site.index').">Clique aqui </a> para ir para a pagina principal";
+Route::fallback(function () {
+    echo "A rota acessada não existe: <a href=" . route('site.index') . ">Clique aqui </a> para ir para a pagina principal";
 });
 
 /*
@@ -52,5 +56,3 @@ Route::get('/contato/{nome}/{caterogia_id}',
         echo "</pre>";
     }
 )->where('caterogia_id','[0-9]+')->where('nome','[A-Za-z]+');*/
-
-
