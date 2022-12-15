afba9d25bb9fda74b404636cb4b36f7dac0a0f86
Jhoni Costa <jhoni_costa@outlook.com>
2022-12-15 10:14:23 -0300

    Alterando a logica de edicao de registro

--- resources/views/app/produto/create.blade.php
+++ resources/views/app/produto/create.blade.php
@@ -10,33 +10,15 @@
         </div>
         <div class="menu">
             <ul>
-                <li><a href="{{ route('produto.index')}}">Voltar</a></li>
+                <li><a href="{{ route('produto.index') }}">Voltar</a></li>
                 <li><a href="">Consulta</a></li>
             </ul>
         </div>
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto">
-                <form action="{{ route('produto.store') }}" method="post">
-                    @csrf
-                    <input type="text" name="nome" value="{{ old('nome') }}" placeholder="Nome" id="" class="borda-preta">
-                    {{ $errors->has('nome') ? $errors->first('nome') : "" }}
-
-                    <input type="text" name="descricao" value="{{ old('descricao') }}" placeholder="Descricao" id="" class="borda-preta">
-                    {{ $errors->has('descricao') ? $errors->first('descricao') : "" }}
+                @component('app.produto._components.form_create_edit', ['unidades' => $unidades])
                     
-                    <input type="text" name="peso" value="{{ old('peso') }}" placeholder="Peso" id="" class="borda-preta">
-                    {{ $errors->has('peso') ? $errors->first('peso') : "" }}
-
-                    <select name="unidade_id">
-                        <option value="">[Selecione]</option>
-                        @foreach($unidades as $unidade)
-                            <option value="{{ $unidade->id }}" {{ old('unidade_id') == $unidade->id ? 'selected' : '' }}>{{ $unidade->descricao }}</option>
-                        @endforeach
-                    </select>
-                    {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : "" }}
-                    
-                    <button type="submit" class="borda-preta">Cadastrar</button>
-                </form>
+                @endcomponent
             </div>
         </div>
     </div>
